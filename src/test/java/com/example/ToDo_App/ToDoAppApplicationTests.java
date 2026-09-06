34239ebfd89b004a3cb4765e5dbb4fec43cc9be8
Sourabh Patil <129728720+SourabhPatil1112000@users.noreply.github.com>
2026-09-06 13:52:53 +0530

    Configure H2 database for ToDoApp tests
    Added H2 database configuration for testing.

--- src/test/java/com/example/ToDo_App/ToDoAppApplicationTests.java
+++ src/test/java/com/example/ToDo_App/ToDoAppApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.ToDo_App;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class ToDoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Test will pass with H2 database
+    }
 
 }
